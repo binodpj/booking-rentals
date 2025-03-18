69d887125bc2248a3d654cfcc2f7f71dae5e3dce
binodprasadjoshi <joshibinodprasad08@gmail.com>
2025-03-18 17:35:40 +0530

    vercel.json added

--- server/index.js
+++ server/index.js
@@ -14,7 +14,13 @@ const app = express();
 
 //middlewares
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.static("public"));
 
